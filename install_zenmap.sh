d2841a2302a039b986438999206b9884bffca94b
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2023-09-24 10:06:33 +0200

    Update install_zenmap.sh

--- install_zenmap.sh
+++ install_zenmap.sh
@@ -10,7 +10,7 @@ apt install alien -y
 # on convertit zenmap.rpm en zenmap.deb
 alien zenmap-7.94-1.noarch.rpm
 
-# download des dépendances fonctionnelles pour zenmap en environnemlent Debian/Ubuntu/kali
+# download des dépendances fonctionnelles pour zenmap en environnement Debian/Ubuntu/kali
 wget http://archive.ubuntu.com/ubuntu/pool/universe/p/pygtk/python-gtk2_2.24.0-5.1ubuntu2_amd64.deb
 wget http://azure.archive.ubuntu.com/ubuntu/pool/universe/p/pygobject-2/python-gobject-2_2.28.6-14ubuntu1_amd64.deb
 wget http://security.ubuntu.com/ubuntu/pool/universe/p/pycairo/python-cairo_1.16.2-2ubuntu2_amd64.deb
@@ -23,6 +23,6 @@ dpkg -i python-gtk2_2.24.0-5.1ubuntu2_amd64.deb
 # installation de zenmap
 dpkg -i zenmap_7.94-2_all.deb
 
-# Suppresion du scripts et programmes résiduels
+# Suppression du script et programmes résiduels
 cd ..
 rm -rf zenmap
