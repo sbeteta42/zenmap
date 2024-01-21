8e8bee7bc862cd4b01549c4c2ff6718c85552f95
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2024-01-21 15:36:17 +0100

    Update install_zenmap.sh

--- install_zenmap.sh
+++ install_zenmap.sh
@@ -1,10 +1,12 @@
 # maj du système
 echo Mise à jour du système... please wait !"
 apt update -y && apt full upgrade -y
+clear
 
 # download de zenmap
 echo "Download de zenmap"
 wget https://nmap.org/dist/zenmap-7.94-1.noarch.rpm
+clear
 
 # installation de alien - convertisseur de paquet RPM en .DEB 
 echo installation de alien - convertisseur de paquet RPM en .DEB"
@@ -19,12 +21,14 @@ echo "download des dépendances fonctionnelles pour zenmap en environnement Debi
 wget http://archive.ubuntu.com/ubuntu/pool/universe/p/pygtk/python-gtk2_2.24.0-5.1ubuntu2_amd64.deb
 wget http://azure.archive.ubuntu.com/ubuntu/pool/universe/p/pygobject-2/python-gobject-2_2.28.6-14ubuntu1_amd64.deb
 wget http://security.ubuntu.com/ubuntu/pool/universe/p/pycairo/python-cairo_1.16.2-2ubuntu2_amd64.deb
+clear
 
 # installation des dépendances fonctionelles
 echo "installation des dépendances fonctionelles" 
 dpkg -i python-gobject-2_2.28.6-14ubuntu1_amd64.deb 
 dpkg -i python-cairo_1.16.2-2ubuntu2_amd64.deb
 dpkg -i python-gtk2_2.24.0-5.1ubuntu2_amd64.deb 
+clear
 
 # installation de zenmap
 echo "installation de zenmap..." 
@@ -33,3 +37,4 @@ dpkg -i zenmap_7.94-2_all.deb
 # Suppression du script et programmes résiduels
 cd ..
 rm -rf zenmap
+echo " Installation de Zenmap terminé !"
